9d136d65e30cb4438e75511d41322fe91b7b8f80
charleskendley478-svg <charleskendley478@gmail.com>
2026-06-06 23:07:00 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function verifier() {
+    let numero = document.getElementById('+509').value;
+    if(numero === ""){
+        alert("+509 !");
+    } else {
+        alert("+509 " + +509 + " anrejistre !");
+    }
+}
